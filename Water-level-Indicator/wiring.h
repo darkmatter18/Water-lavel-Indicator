122637c81e4e147705234df11c8a1c80ac7d9268
Arkadip <in2arkadipb13@gmail.com>
2021-03-19 22:35:49 +0530

    docs: wiring.h docs done

--- Water-level-Indicator/wiring.h
+++ Water-level-Indicator/wiring.h
@@ -2,19 +2,39 @@
  * Define all the pins
  * Pin Used Interrupts (2, 3), LCD (4, 5, 6, 7, 8, 9), DHT11 (10), Ultra Sonic Sensor (11, 12), LCD Backlit (A0), Buzzer (A1)
  */
-// LCD pins
+/**
+ * @brief LCD configuration
+ */
+
+// LCD I2C Address
 #define LCD_I2C_ADDR 0x27
+
+// LCD Width
 #define LCD_WIDTH 16
+
+// LCD Height
 #define LCD_HEIGHT 2
 
-// Pins for sensors
+/**
+ * @brief Sensor Pins configuration
+ */
+
+// DHT pin
 #define DHT_PIN 9
-#define TRIGGER_PIN 5
-#define ECHO_PIN 4
+
+// Trigger pin for ultrasonic sensor
+#define TRIGGER_PIN 6
+
+// Echo Pin for ultrasonic sensor
+#define ECHO_PIN 5
 
 //Buzzer pin
 #define BUZZER_PIN A1
 
-// Interupt Pins
+// Interrupt Pins
+
+// Buzzer interrupt
 #define BUZZER_INT 2
+
+// Self Stop interrupt
 #define SELF_STOP_INT 3
