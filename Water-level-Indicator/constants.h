3361e64d9074cb9c44c59ba6bcdb78c9a684a36a
Arkadip <in2arkadipb13@gmail.com>
2021-03-21 20:58:37 +0530

    docs: Line wise docs added constent.h

--- Water-level-Indicator/constants.h
+++ Water-level-Indicator/constants.h
@@ -1,12 +1,10 @@
 #define SERIAL_DEBUG 1 //Mode for serial debug, 0 -> No debug, 1 -> Debug over serial is on
 
 // Constants for distances
-#define SPEED_OF_SOUND 0.034 // unit(cm/us)
-#define ITER_M 10
+#define SPEED_OF_SOUND 0.034 // speed of sound(cm/us)
+#define ITER_M 10 // Interation for median checking
 
 // Tank dimention
 #define TANK_BUTTOM_DISTANCE 104    // Distance of the water lavel, when the tank is empty
 #define TANK_TOP_DISTANCE 25    // Distance of the water lavel, when the tank is full
 #define TANK_REDIUS 50
-
-#define MAX_DELAY_ON_ENGINE_OFF 600000UL
